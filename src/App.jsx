72874a9b15fc81e8ae81b198dc669584e9c70252
adityafajarsy <adityafajar.sy90@gmail.com>
2025-11-05 16:45:35 +0700

    feat: add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import gsap from "gsap"
-import { ScrollTrigger, SplitText } from "gsap/all"
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
 
-gsap.registerPlugin(ScrollTrigger, SplitText)
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
-
   return (
-    <div className="flex-center h-screen">
-      <h1 className="text-3xl text-indigo-300">Hello, GSAP!</h1>
-    </div>
-  )
+    <main>
+      <Navbar />
+    </main>
+  );
 }
 
-export default App
+export default App;
